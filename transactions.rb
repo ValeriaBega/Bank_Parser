0190816e59c2ecc16b43c9b17732ba625d89875d
begavaleria <begavaleria13@gmail.com>
2019-05-08 23:30:14 +0300

    Revert "initial commit"
    This reverts commit 45cda75bcd9f38f71951cdf636ac69b6e5be188f.

--- transactions.rb
+++
@@ -1,9 +0,0 @@
-class Transactions
-	attr_accessor :date, :description, :amount
-
-	def initialize(date, description, amount)
-		@date        = date
-		@description = description
-		@amount      = amount
-	end	
-end
