45cda75bcd9f38f71951cdf636ac69b6e5be188f
begavaleria <begavaleria13@gmail.com>
2019-05-08 22:54:06 +0300

    initial commit

---
+++ spec/victoria_bank_md_spec.rb
@@ -0,0 +1,32 @@
+require 		 './victoria_bank'
+require_relative 'spec_helper.rb'
+require 'open-uri'
+
+describe Victoria_bank_md do
+	let(:subject) { Victoria_bank_md.new }
+
+	context 'Tests single account' do
+		let(:nokogiri) { Nokogiri::HTML }
+		let(:html) 	   { nokogiri.fragment(File.read("spec/fixtures/accounts.html")) }
+		it '#parse_acc' do
+			account = subject.parse_acc(html)
+
+			expect(account.name).to eq     "MD15VI225911807270111111"
+			expect(account.balance).to eq  "11,111.11"
+			expect(account.currency).to eq "MDL"
+			expect(account.nature).to eq   "Card Account"
+		end
+	end
+
+	context 'Tests tranaction' do
+		let(:nokogiri) { Nokogiri::HTML }
+		let(:html) 	   { nokogiri.fragment(File.read("spec/fixtures/transaction.html")) }
+		it '#parse_transaction' do
+			transaction = subject.parse_transaction(html)
+
+			expect(transaction.date).to eq        "2019-05-02"
+			expect(transaction.description).to eq "Retail LUKOIL PECO-13 CHISINAU MOLDOVA"
+			expect(transaction.amount).to eq      "946.03"
+		end
+	end
+end
---
+++ transactions.rb
@@ -0,0 +1,9 @@
+class Transactions
+	attr_accessor :date, :description, :amount
+
+	def initialize(date, description, amount)
+		@date        = date
+		@description = description
+		@amount      = amount
+	end	
+end
