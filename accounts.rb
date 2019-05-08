5eca459b6fcbc943202b9f1f75ec7d90f972d504
begavaleria <begavaleria13@gmail.com>
2019-05-08 23:36:53 +0300

    add new parser

---
+++ accounts.rb
@@ -0,0 +1,13 @@
+require_relative 'transactions.rb'
+
+class Accounts
+	attr_accessor :name, :currency, :balance, :nature, :transactions
+
+	def initialize(name, currency, balance, nature)
+		@name         = name
+		@currency     = currency
+		@balance      = balance
+		@nature       = nature
+		@transactions = [transactions]
+	end
+end
