c0ac73c2df829455a35aeacd0d2724d330f9f3d6
begavaleria <begavaleria13@gmail.com>
2019-05-10 18:35:08 +0300

    indentation changes

--- accounts.rb
+++ accounts.rb
@@ -1,13 +1,12 @@
 require_relative 'transactions.rb'
 
 class Accounts
-    attr_accessor :name, :currency, :balance, :nature, :transactions
-
-    def initialize(name, currency, balance, nature)
-        @name         = name
-        @currency     = currency
-        @balance      = balance
-        @nature       = nature
-        @transactions = [transactions]
-    end
+  attr_accessor :name, :currency, :balance, :nature, :transactions
+  def initialize(name, currency, balance, nature)
+    @name         = name
+    @currency     = currency
+    @balance      = balance
+    @nature       = nature
+    @transactions = [transactions]
+  end
 end
